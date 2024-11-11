2de90a0d390854dccf6eea6e9461e7e0717ccc4b
Ameer Nur Saidy <ansnursaidy@gmail.com>
2024-11-10 20:19:29 -0800

    Added Recipe Information Bulk

--- src/recipe-app/src/RecipeModel.jsx
+++ src/recipe-app/src/RecipeModel.jsx
@@ -1,19 +1,43 @@
 const API_URL = 'https://api.spoonacular.com/recipes';
 //const API_KEY = process.env.REACT_APP_SPOONACULAR_API_KEY;
-
+const API_KEY = '';
 
 import axios from 'axios';
 
 
 export const getRecipes= async (query) =>{
     try{
-        const res = await axios.get(`${API_URL}/complexSearch?apiKey=${API_KEY}&query=${query}`)
-        //console.log(res.data.results);
-        return res.data.results;
+        //Only returns us the id, title, and id
+        const res = await axios.get(`${API_URL}/complexSearch?apiKey=${API_KEY}&query=${query}&number=2`)
+        //recipes without information & grab the ids
+        const recipes = res.data.results; 
+        const recipeIds = recipes.map(recipe => recipe.id);
+
+        //recipes with information (pass the ids)
+         const recipeInfo = await getRecipeInfoBulk(recipeIds);
+
+        const modifiedRecipes = recipes.map((recipe, index) => ({
+          ...recipe, //copy elements into a new object
+          information: recipeInfo[index],  // Attach detailed information
+         }));
+         console.log(modifiedRecipes);
+        return modifiedRecipes;
     }catch(err){
         console.error('Error getting recipes:', err);
         res.status(500).json({ err: 'Internal server error' });
     } 
+}
+
+export const getRecipeInfoBulk = async (recipeIds) =>{
+
+    try{
+        const ids = recipeIds.join(',');
+        const res = await axios.get(`${API_URL}/informationBulk?apiKey=${API_KEY}&ids=${ids}`);
+        return res.data;
+    }catch(err){
+        console.error('Error getting recipe information bulk:', err);
+        res.status(500).json({ err: 'Internal server error' });
+    }
 }
 
 export const loadSavedRecipes= () => {
