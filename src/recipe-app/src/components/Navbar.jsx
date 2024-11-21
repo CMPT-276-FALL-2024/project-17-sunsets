cc3528d261df026555f486139350bb4bf220dcfb
Simon D <doannhandv1230@gmail.com>
2024-11-20 18:07:09 -0800

    Set up favorite page

--- src/recipe-app/src/components/Navbar.jsx
+++ src/recipe-app/src/components/Navbar.jsx
@@ -11,7 +11,7 @@ function Navbar({className}) {
         </span>
         <Link to="/" className="home-active">Home</Link>
         <Link to="/recipes" className = "recipe-active">Recipe</Link> {/* Corrected route path */}
-        <Link to="/favorites">Favorites</Link>
+        <Link to="/favorites" className = "favorite-active">Favorites</Link>
         <Link to="/mealplanner">Meal Planner</Link>
         {/* <span className="profile-icon">
           <i className="fa-regular fa-user"></i>
---
+++ src/recipe-app/src/pages/FavoritePage.jsx
@@ -0,0 +1,66 @@
+// Recipes.jsx
+import Navbar from '../components/Navbar.jsx';
+import Footer from '../components/Footer.jsx';
+import SearchBar from '../components/Recipe_SearchBar.jsx';
+import '../styles/RecipePage.css';
+import '../styles/HomePage.css';
+import '../styles/FavoritePage.css';
+import food from '../assets/images/food.jpg';
+
+
+const FavoritePage = () =>{
+
+    return (
+        <div className="page-container">
+            <Navbar className="favorite" />
+            <SearchBar className="recipe" />
+                
+            <div className="favorite-content">
+                <h1 className="favorite-title">Your Favorite Recipes: </h1>  
+                             
+                <div id = "recipe-meal">
+
+                    {/* The content below just use for testing the layout  */}
+                    <div className={`home-meal-item`}>
+                        <div className={`home-meal-img`}>
+                            <img src={food} alt="potato" />
+                        </div>
+                        <div className={`home-meal-name`}>
+                            <button className={`home-btn`}>
+                                <i className="far fa-heart"></i>
+                            </button>
+                            <h3>Potato</h3>
+                        </div>
+                    </div>
+                    <div className={`home-meal-item`}>
+                        <div className={`home-meal-img`}>
+                            <img src={food}  alt="potato" />
+                        </div>
+                        <div className={`home-meal-name`}>
+                            <button className={`home-btn`}>
+                                <i className="far fa-heart"></i>
+                            </button>
+                            <h3>Potato</h3>
+                        </div>
+                    </div>
+                    <div className={`home-meal-item`}>
+                        <div className={`home-meal-img`}>
+                            <img src={food}  alt="potato" />
+                        </div>
+                        <div className={`home-meal-name`}>
+                            <button className={`home-btn`}>
+                                <i className="far fa-heart"></i>
+                            </button>
+                            <h3>Potato</h3>
+                        </div>
+                    </div>
+
+
+                </div>
+            </div>
+                
+            <Footer className="home" />
+        </div>
+        );
+};
+export default FavoritePage;
