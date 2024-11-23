e6d578599da6bbec7e625dabdcddd37e59d610fa
ChaitanyaMittal27 <chaitanyamittal27@gmail.com>
2024-11-22 20:10:51 -0800

    added tutorial.jsx page

--- src/recipe-app/src/pages/Tutorials.jsx
+++ src/recipe-app/src/pages/Tutorials.jsx
@@ -50,7 +50,6 @@ const Tutorials = () => {
               width="100%"
               height="400"
               src={`https://www.youtube.com/embed/${currentVideo}`}
-              frameBorder="0"
               allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
               allowFullScreen
             ></iframe>
