ee2d9d36beaa6b8aa067b36f6bcae6d167fa2c70
ChaitanyaMittal27 <chaitanyamittal27@gmail.com>
2024-11-22 20:46:41 -0800

    integrated changes, added basic css for testing

--- src/recipe-app/src/YouTubeModel.jsx
+++ src/recipe-app/src/YouTubeModel.jsx
@@ -1,5 +1,5 @@
 const API_URL = "https://www.googleapis.com/youtube/v3/search";
-const API_KEY = "YOUR_YOUTUBE_API_KEY"; // Replace with your API key
+const API_KEY = ""; // Replace with your API key
 
 import axios from "axios";
 
